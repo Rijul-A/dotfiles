7543c70dffa7ec18b7da162ff320f9446b133b3c
Rijul-A <31570722+Rijul-A@users.noreply.github.com>
2021-12-01 20:37:25 +0530

    Source files from home directory

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,9 +1,9 @@
 # this will download everything, if it does not exist
-if [[ ! -e .zsh/antigen.zsh ]]; then
+if [[ ! -e ~/.zsh/antigen.zsh ]]; then
   echo "Dowloading missing antigen.zsh"
-  curl --silent -L git.io/antigen > .zsh/antigen.zsh
+  curl --silent -L git.io/antigen > ~/.zsh/antigen.zsh
 fi
-source .zsh/antigen.zsh
+source ~/.zsh/antigen.zsh
 antigen use oh-my-zsh
 antigen bundle adb
 antigen bundle git
@@ -17,15 +17,15 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen theme romkatv/powerlevel10k
 antigen apply
-if [[ ! -e .zsh/config.zsh ]]; then
+if [[ ! -e ~/.zsh/config.zsh ]]; then
   echo "Downloading missing config.zsh"
-  curl --silent -L https://raw.githubusercontent.com/Rijul-A/manjaro-zsh-config/master/manjaro-zsh-config > .zsh/config.zsh
+  curl --silent -L https://raw.githubusercontent.com/Rijul-A/manjaro-zsh-config/master/manjaro-zsh-config > ~/.zsh/config.zsh
 fi
-source .zsh/config.zsh
-if [[ ! -e .zsh/p10k.zsh ]]; then
+source ~/.zsh/config.zsh
+if [[ ! -e ~/.zsh/p10k.zsh ]]; then
   echo "Downloading missing p10k.zsh"
-  curl --silent -L https://raw.githubusercontent.com/Rijul-A/manjaro-zsh-config/master/p10k.zsh > .zsh/p10k.zsh
+  curl --silent -L https://raw.githubusercontent.com/Rijul-A/manjaro-zsh-config/master/p10k.zsh > ~/.zsh/p10k.zsh
 fi
-source .zsh/p10k.zsh
+source ~/.zsh/p10k.zsh
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
--- .zshrc
+++ .zshrc
@@ -1,5 +1,5 @@
 setopt histignorespace  # not used upstream
-source .zsh/init.zsh
+source ~/.zsh/init.zsh
 
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias zsh_reset="rm -rf ~/.zsh/config.zsh ~/.zsh/antigen.zsh"
